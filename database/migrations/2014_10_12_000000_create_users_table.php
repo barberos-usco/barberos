ec65298602e6fd71806a57fe94e11855e02e7f1d
Helmer <thetorrez5@hotmail.com>
2020-05-06 15:34:02 -0500

    Le hice el auth, ya está la tabla de role, ya está relacionado con users, y está creado los enlaces principales como create, update, destroy...

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,17 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('apellidos');
+            $table->string('genero');
+            $table->date('fecha_nacimiento');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role_id')->index();
             $table->rememberToken();
+            $table->string('ubicacion');
+            $table->string('url_wallpa');
+            $table->string('url_fotoPerfil');
             $table->timestamps();
         });
     }
