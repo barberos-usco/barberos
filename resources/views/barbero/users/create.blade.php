844d100ebe9be4807f7a785bd2b57775af5d6585
Helmer <thetorrez5@hotmail.com>
2020-05-07 21:44:30 -0500

    Puse barra arriba, color café, con buscador y en el create, lo puse en el centro, con divs, bonito, falta cambiar el fondo

--- resources/views/barbero/users/create.blade.php
+++ resources/views/barbero/users/create.blade.php
@@ -1,110 +1,107 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+@extends('layouts.app')
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Crear Usuario</div>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <h1>Para crear un usuario, no lo usaré igual </h1>
-    {!! Form::open(['method'=>'POST', 'action'=>'BarberoUsersController@store', 'files'=>true]) !!}
-        <table>
-            <tr>
-                <td>
-                    {!! Form::label('name', 'Nombre: ') !!}
-                </td>
-                <td>
-                    {!! Form::text('name') !!}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    {!! Form::label('apellidos', 'Apellidos: ') !!}
-                </td>
-                <td>
-                    {!! Form::text('apellidos') !!}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    {!! Form::label('genero', 'Género: ') !!}
-                </td>
-                <td>
-                    {!! Form::select('genero', ['Femenino' => 'Femenino', 'Masculino' => 'Masculino']) !!}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    {!! Form::label('fecha_nacimiento', 'Fecha Nacimiento: ') !!}
-                </td>
-                <td>
-                    {!! Form::date('fecha_nacimiento') !!}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    {!! Form::label('password', 'Password: ') !!}
-                </td>
-                <td>
-                    {!! Form::password('password') !!}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    {!! Form::label('email', 'E-Mail: ') !!}
-                </td>
-                <td>
-                    {!! Form::text('email') !!}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    {!! Form::label('email', 'Verificar E-Mail: ') !!}
-                </td>
-                <td>
-                    {!! Form::text('email_verified_at') !!}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    {!! Form::label('role_id', 'Role: ') !!}
-                </td>
-                <td>
-                    {!! Form::text('role_id') !!}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    {!! Form::label('ubicacion', 'Ubicación: ') !!}
-                </td>
-                <td>
-                    {!! Form::text('ubicacion') !!}
-                </td>
-            </tr>
-            
-            
-            <tr>
-                <td>
-                    {!! Form::submit('Crear') !!}
-                </td>
-                <td>
-                    {!! Form::reset('Borrar Campos') !!}
-                </td>
-            </tr>
-            
+                <div class="card-body">
 
-        </table>
-    {!! Form::close() !!}
-</body>
-</html>
+                    
+                    {!! Form::open(['method'=>'POST', 'action'=>'BarberoUsersController@store', 'files'=>true]) !!}
+                        <table>
+                            <tr>
+                                <td>
+                                    {!! Form::label('name', 'Nombre: ') !!}
+                                </td>
+                                <td>
+                                    {!! Form::text('name') !!}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {!! Form::label('apellidos', 'Apellidos: ') !!}
+                                </td>
+                                <td>
+                                    {!! Form::text('apellidos') !!}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {!! Form::label('genero', 'Género: ') !!}
+                                </td>
+                                <td>
+                                    {!! Form::select('genero', ['Femenino' => 'Femenino', 'Masculino' => 'Masculino']) !!}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {!! Form::label('fecha_nacimiento', 'Fecha Nacimiento: ') !!}
+                                </td>
+                                <td>
+                                    {!! Form::date('fecha_nacimiento') !!}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {!! Form::label('password', 'Password: ') !!}
+                                </td>
+                                <td>
+                                    {!! Form::password('password') !!}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {!! Form::label('email', 'E-Mail: ') !!}
+                                </td>
+                                <td>
+                                    {!! Form::text('email') !!}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {!! Form::label('email', 'Verificar E-Mail: ') !!}
+                                </td>
+                                <td>
+                                    {!! Form::text('email_verified_at') !!}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {!! Form::label('role_id', 'Role: ') !!}
+                                </td>
+                                <td>
+                                    {!! Form::text('role_id') !!}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {!! Form::label('ubicacion', 'Ubicación: ') !!}
+                                </td>
+                                <td>
+                                    {!! Form::text('ubicacion') !!}
+                                </td>
+                            </tr>
+                            
+                            
+                            <tr>
+                                <td>
+                                    {!! Form::submit('Crear') !!}
+                                </td>
+                                <td>
+                                    {!! Form::reset('Borrar Campos') !!}
+                                </td>
+                            </tr>
+                            
+
+                        </table>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Home</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -18,17 +18,19 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/barbero.css') }}" rel="stylesheet">
 </head>
-<body>
+<body >
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark shadow-sm" style="background-color: #4E270C;">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    BarbApp
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
@@ -38,6 +40,15 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <!-- Barra de búsqueda -->
+
+                        <nav class="navbar navbar-dark " style="background-color: #4E270C;">
+                            <form class="form-inline">
+                              <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+                              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                            </form>
+                        </nav>
+
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
@@ -58,7 +69,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Cerrar Sesión') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
