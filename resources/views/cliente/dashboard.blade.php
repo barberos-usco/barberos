5304ac8b05155a6a7009e73ffb6c862eb89c11b6
Byakoxx <thetorrez5@hotmail.com>
2020-07-23 17:08:38 -0500

    Add: Tarjetas ya terminadas con estilo y redireccionamiento. -Helmer

--- resources/views/cliente/dashboard.blade.php
+++ resources/views/cliente/dashboard.blade.php
@@ -1,34 +1,56 @@
 <img src="{{ asset('/images/Logo.jpg') }}" class="img-fluid  radio_logo sombreado " alt="Responsive image" width="1100" >
 <div class="caja">
-    <div class="container colum3">
-    	<div class="container">
-                <div class="section-title">
-                    <h2>Información del Barbero</h2>
-                </div>
-                <div >
-                    
-                        @if ($users)
-                            @foreach ($users as $user)
-                                <tr>
-                                    <div class="card" style="width: 15rem;">
-                                      <img src="{{ asset('/images/profile-img.jpg') }}" class="img-fluid  radio_logo sombreado " alt="Responsive image" width="10%" >
-                                        <div class="card-body">
-                                            <h5 class="card-title"><b>{{ Auth::user()->name ." " . Auth::user()->apellidos  }}</b></h5>
-                                            <p class="card-text"> {{ $user->id }}</p>
-                                        </div>
-                                        <div class="card-footer">
-                                            <a href="{{ route('barbero.users.perfil', $user->id) }}" class="btn color-botom-2" >Go somewhere</a>
-                                        </div>
-                                      </div>
-                                </tr>
-
-                            @endforeach
-                        @endif
-
-                    
-                </div>
-            </div>
+    
+        <div class="row">
             
+            
+                @if ($users)
+                    @foreach ($users as $user)
+                    
+                        <div class="card-home column4" style="">
+                            <img src="{{ asset('/images/profile-img.jpg') }}" class="img-fluid  radio_logo sombreado tamaño-home "  >
+                            <div class="card-body">
+                                <h5 class="card-title text-center" style="color: #352c23;"><b>{{ $user->name ." " . $user->apellidos  }}</b></h5>
+                                <div class="border-top my-3"></div>
+                                <p class="card-text"> <div class="valoracion2">
+                                    
+                                    <!-- La idea es porner un foreach para que se haga las estrellas que tenga en la valoracion ;) -->
 
-    </div>
+                                    <!-- Estrella 1 -->
+                                    <button>
+                                        <i class="fas fa-star "></i>
+                                    </button>
+                    
+                                    <!-- Estrella 2 -->
+                                    <button>
+                                        <i class="fas fa-star"></i>
+                                    </button>
+                    
+                                    <!-- Estrella 3 -->
+                                    <button>
+                                        <i class="fas fa-star "></i>
+                                    </button>
+                    
+                                    <!-- Estrella 4 -->
+                                    <button>
+                                        <i class="fas fa-star "></i>
+                                    </button>
+                    
+                                    <!-- Estrella 5 -->
+                                    <button>
+                                        <i class="fas fa-star "></i>
+                                    </button>
+                                    
+                                    
+                                </div></p>
+                            </div>
+                            <div class="card-footer-home">
+                                <a href="{{ route('barbero.users.perfil', $user->id) }}" class="btn color-botom-home text-center" >Perfil</a>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+                
+        </div>
+    
 </div>
