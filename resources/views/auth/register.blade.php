4d04a0047f08a25655cbe0d7b24b9d27f43380c7
Byakoxx <thetorrez5@hotmail.com>
2020-07-23 10:07:26 -0500

    Add: adicion de los campos apellidos y rol en registro. -Helmer

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,27 @@
-@extends('layouts.app')
+@extends('layouts.main')
+
+{{-- @include('barbero.users.barra-lateral') --}}
+@section('title','Registro')
+
+
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Registro') }}</div>
+            <div class="card-registro sombreado">
+                <div class="card-header-registro letras sombreado"><h2> {{ __('Registro') }}</h2></div>
 
-                <div class="card-body">
+                <div class="card-body-registro letras sombreado">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
+                        <br>
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre: ') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control redondeo sombreado @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -25,11 +31,20 @@
                             </div>
                         </div>
 
-      
 
-
-
-
+                        <div class="form-group row {{ $errors->has('apellidos') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label text-md-right">{{ __('Apellidos') }}</label>
+                        
+                            <div class="col-md-6">
+                                <input type="text" class="form-control redondeo sombreado " name="apellidos" value="{{ old('apellidos') }}">
+                        
+                                @if ($errors->has('apellidos'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('apellidos') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
 
 
@@ -37,7 +52,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control redondeo sombreado @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -51,7 +66,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control redondeo sombreado @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -65,24 +80,29 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirme Contraseña') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control redondeo sombreado" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
-                        <!--<div class="form-group row">
-                            <label for="rol" class="col-md-4 col-form-label text-md-right">{{ __('Eres:') }}</label>
+                       <div class="form-group row">
+                            <label for="role_id" class="col-md-4 col-form-label text-md-right">{{ __('Eres:') }}</label>
 
                             <div class="col-md-6">
-                                <select id="rol" type="text" class="form-control" name="rol">
-                                    <option>Barbero</option>
-                                    <option>Cliente</option>
+                                <select type="text" class="form-control redondeo" name="role_id">
+                                    <option value="1">Barbero</option>
+                                    <option value="2">Cliente</option>
                                 </select>
+                                @if ($errors->has('role_id'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('role_id') }}</strong>
+                                    </span>
+                                @endif
                             </div>
-                        </div>-->
+                        </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn color-botom sombreado">
                                     {{ __('Registrar') }}
                                 </button>
                             </div>
