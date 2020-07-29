5606420bf92376e80b6f0dbc6b1ddd0ad0b961cb
Byakoxx <thetorrez5@hotmail.com>
2020-07-29 16:00:31 -0500

    Add: Modal de comentarios segun el role. -Helmer

--- resources/views/barbero/users/perfil.blade.php
+++ resources/views/barbero/users/perfil.blade.php
@@ -117,38 +117,71 @@
         <h2>Ubicación</h2><br>
         <iframe class="mapa_google sombreado" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15938.151498328249!2d-75.3062321!3d2.948114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sco!4v1594932655980!5m2!1ses!2sco"  allowfullscreen="" aria-hidden="true" tabindex="0"></iframe><br><br>
         <h2>Comentarios</h2>
-        <div class="comentarios letras-negras btn sombreado" data-toggle="modal" data-target="#myModal">Click para ver los comentarios.</div>
-        <!-- The Modal -->
-        <div class="modal" id="myModal">
-            <div class="modal-dialog">
-                <div class="modal-content">
+        @if(Auth::user()->role_id == 1)
+            <div class="comentarios letras-negras btn sombreado" data-toggle="modal" data-target="#myModal">Click para ver los comentarios.</div>
+            <!-- The Modal para barberos -->
+            <div class="modal" id="myModal">
+                <div class="modal-dialog">
+                    <div class="modal-content">
 
-                    <!-- Modal Header -->
-                    <div class="modal-header">
-                    <h4 class="modal-title">Comentarios</h4>
-                    <button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
-                    </div>
-
-                    <!-- Modal body -->
-                    <div class="modal-body">
-                        <div class="comentarios letras-negras sombreado">
-                            <b>Andrés: </b><br>
-                            Me gustó el pelukeado, buen servicio.
+                        <!-- Modal Header -->
+                        <div class="modal-header">
+                        <h4 class="modal-title">Comentarios</h4>
+                        <button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
                         </div>
-                        <div class="comentarios letras-negras sombreado">
-                            <b>Camilo: </b><br>
-                            Un patán, me trasquiló, cero estrellas.
+
+                        <!-- Modal body -->
+                        <div class="modal-body">
+                            <div class="comentarios letras-negras sombreado">
+                                <b>Andrés: </b><br>
+                                Me gustó el pelukeado, buen servicio.
+                            </div>
+                            <div class="comentarios letras-negras sombreado">
+                                <b>Camilo: </b><br>
+                                Un patán, me trasquiló, cero estrellas.
+                            </div>
                         </div>
-                    </div>
 
-                    <!-- Modal footer -->
-                    <div class="modal-footer ">
-                    <button type="button" class="btn color-botom-2 sombreado" data-dismiss="modal">Cerrar</button>
-                    </div>
+                        <!-- Modal footer -->
+                        <div class="modal-footer ">
+                        <button type="button" class="btn color-botom-2 sombreado" data-dismiss="modal">Cerrar</button>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
-        </div><br>
+            </div><br>
+        @else
+            <div class="comentarios letras-negras btn sombreado" data-toggle="modal" data-target="#myModal">Click para hacer un comentario.</div>
+            <!-- The Modal para clientes -->
+            <div class="modal" id="myModal">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+
+                        <!-- Modal Header -->
+                        <div class="modal-header">
+                        <h4 class="modal-title">Comentario</h4>
+                        <button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
+                        </div>
+
+                        <!-- Modal body -->
+                        <div class="modal-body">
+                            <div class="comentarios letras-negras sombreado"><br>
+                                <b>{{ Auth::user()->name }}: </b><br><br>
+                                {!! Form::textarea('Depilacion_cera',null,['class'=>'form-control caja-texto']) !!}
+                            </div>
+                            
+                        </div>
+
+                        <!-- Modal footer -->
+                        <div class="modal-footer ">
+                        <button type="button" class="btn color-botom-home sombreado" data-dismiss="modal">Cerrar</button>
+                        <button type="button" class="btn color-botom-home sombreado" data-dismiss="modal">Guardar</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div><br>
+        @endif
 
         <div><br>
             <h2>Valoración</h2>
