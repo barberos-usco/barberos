85c5592187aaa8463ecddc25f808c73117c4dcc5
Byakoxx <thetorrez5@hotmail.com>
2020-08-14 14:19:56 -0500

    Add: Modals para barra lateral, los llama en BL pero la accion esta en footer. -Helmer

--- resources/views/partials/barra-lateral.blade.php
+++ resources/views/partials/barra-lateral.blade.php
@@ -10,21 +10,70 @@
                     <img src="{{ asset('images/'.Auth::user()->url_fotoPerfil ) }}" alt="" class="img-fluid rounded-circle " style="width:140px; height:170px;">
                     @endif
       
-      <h1 class="text-light"><a href="{{ route('perfil', Auth::user()->id) }}"> {{ Auth::user()->name . " " .Auth::user()->apellidos}}</a></h1>
+      <h1 class="text-light"><a href="{{ route('perfil', Auth::user()->id) }}"> {{ Auth::user()->name . " " .Auth::user()->apellidos}}</a> <div class="btn color-manual redondeo sombreado" data-toggle="modal" data-target="#myModal01"> ?</div>
+        <!-- The Modal para barberos -->
+        <div class="modal" id="myModal01">
+            <div class="modal-dialog modal-large">
+                <div class="modal-content letras ">
+
+                    <!-- Modal Header -->
+                    <div class="modal-header">
+                    <h4 class="modal-title"><b>Usuario</b></h4>
+                    <button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
+                    </div>
+
+                    <!-- Modal body -->
+                    <div class="modal-body text-justify">
+                      <h4><p>Ya iniciado sesión, se puede observar el nombre del mismo y la imagen de perfil.</p></h4>
+                    </div>
+
+                    <!-- Modal footer -->
+                    <div class="modal-footer ">
+                    <button type="button" class="btn color-botom-home-inline sombreado" data-dismiss="modal">Cerrar</button>
+                    </div>
+
+                </div>
+            </div>
+        </div></h1>
 
     </div>
 
     <nav class="nav-menu">
       <ul>
-        <li><a href="{{ route('home') }}"><span style="font-size: 1.2em; color: #463125;"><i class="	fas fa-home"></i></span> <span>Inicio</span></a></li>
-        @if(Auth::user()->role_id === 1)
-          <li><a href="{{ route('barberos') }}"><span style="font-size: 1.2em; color: #463125;"><i class="  fas fa-users"></i></span> <span>Barberos</span></a></li>
-        @endif
-        <li><a href="{{ route('perfil', Auth::user()->id) }}"><span style="font-size: 1.2em; color: #463125;"><i class="	fas fa-user-alt"></i></span> <span>Perfil</span></a></li>
-        @if(Auth::user()->role_id === 1)
-          <li><a href="{{ route('barbero.users.edit', Auth::user()->id) }}"><span style="font-size: 1.2em; color: #463125;"><i class="	fas fa-edit"></i></span>Editar Perfil</a></li>
+        <li><a href="{{ route('home') }}"><span style="font-size: 1.2em; color: #463125;"><i class="	fas fa-home"></i></span> <span>Inicio</span></a>
+        @if(Auth::user()->role_id == 1)
+          <div class="btn color-manual-Inicio redondeo sombreado" data-toggle="modal" data-target="#myModal02"> ?</div>
+          
         @else
-          <li><a href="{{ route('cliente.users.edit', Auth::user()->id) }}"><span style="font-size: 1.2em; color: #463125;"><i class="	fas fa-edit"></i></span>Editar Perfil</a></li>
+          <div class="btn color-manual-Inicio redondeo sombreado" data-toggle="modal" data-target="#myModal002"> ?</div>
+        @endif</li>
+        @if(Auth::user()->role_id === 1)
+          <li><a href="{{ route('barberos') }}"><span style="font-size: 1.2em; color: #463125;"><i class="  fas fa-users"></i></span> <span>Barberos</span></a>
+            @if(Auth::user()->role_id == 1)
+              <div class="btn color-manual-Inicio redondeo sombreado" data-toggle="modal" data-target="#myModal03"> ?</div>
+            @endif
+          </li>
+        @endif
+        <li><a href="{{ route('perfil', Auth::user()->id) }}"><span style="font-size: 1.2em; color: #463125;"><i class="	fas fa-user-alt"></i></span> <span>Perfil</span></a>
+          @if(Auth::user()->role_id == 1)
+            <div class="btn color-manual-Inicio redondeo sombreado" data-toggle="modal" data-target="#myModal04"> ?</div>
+            
+          @else
+            <div class="btn color-manual-Inicio redondeo sombreado" data-toggle="modal" data-target="#myModal004"> ?</div>
+          @endif
+        </li>
+        @if(Auth::user()->role_id === 1)
+          <li><a href="{{ route('barbero.users.edit', Auth::user()->id) }}"><span style="font-size: 1.2em; color: #463125;"><i class="	fas fa-edit"></i></span>Editar Perfil</a>
+            @if(Auth::user()->role_id == 1)
+              <div class="btn color-manual-Inicio redondeo sombreado" data-toggle="modal" data-target="#myModal05"> ?</div>
+            @endif
+          </li>
+        @else
+          <li><a href="{{ route('cliente.users.edit', Auth::user()->id) }}"><span style="font-size: 1.2em; color: #463125;"><i class="	fas fa-edit"></i></span>Editar Perfil</a>
+            @if(Auth::user()->role_id == 2)
+              <div class="btn color-manual-Inicio redondeo sombreado" data-toggle="modal" data-target="#myModal005"> ?</div>
+            @endif
+          </li>
         @endif
         
         
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,3 +2,184 @@
     <p>Contáctanos<br>
     Correo: thetorrez5@hotmail.com  Teléfono: 316-244-92-49</p>
  </footer>
+
+ <!-- Modales de la barra lateral -->
+	
+	
+	<!-- Modal Inicio para barbero (Dashboard) -->
+	<div class="modal" id="myModal02">
+		<div class="modal-dialog modal-large">
+			<div class="modal-content letras ">
+
+				<!-- Modal Header -->
+				<div class="modal-header">
+				<h4 class="modal-title"><b>Inicio</b></h4>
+				<button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
+				</div>
+
+				<!-- Modal body -->
+				<div class="modal-body text-justify">
+				  <h4><p>Como usuario-barbero, éste enlace lo llevará al dashboard y podrá hacer gestión de sus imagenes y demás.</p></h4>
+				</div>
+
+				<!-- Modal footer -->
+				<div class="modal-footer ">
+				<button type="button" class="btn color-botom-home-inline sombreado" data-dismiss="modal">Cerrar</button>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Modal Inicio para cliente (barberos) -->
+	<div class="modal" id="myModal002">
+		<div class="modal-dialog modal-large">
+			<div class="modal-content letras ">
+
+				<!-- Modal Header -->
+				<div class="modal-header">
+				<h4 class="modal-title"><b>Inicio</b></h4>
+				<button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
+				</div>
+
+				<!-- Modal body -->
+				<div class="modal-body text-justify">
+				  <h4><p>Como usuario-cliente, éste enlace lo llevará a la lista de barberos registrados.</p></h4>
+				</div>
+
+				<!-- Modal footer -->
+				<div class="modal-footer ">
+				<button type="button" class="btn color-botom-home-inline sombreado" data-dismiss="modal">Cerrar</button>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	
+	
+	<!-- Modal Barberos para barberos -->
+	<div class="modal" id="myModal03">
+		<div class="modal-dialog modal-large">
+			<div class="modal-content letras ">
+
+				<!-- Modal Header -->
+				<div class="modal-header">
+				<h4 class="modal-title"><b>Barberos</b></h4>
+				<button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
+				</div>
+
+				<!-- Modal body -->
+				<div class="modal-body text-justify">
+				  <h4><p>Como usuario-barbero, éste enlace lo llevará a la lista de barberos registrados, no aparecerá su perfil.</p></h4>
+				</div>
+
+				<!-- Modal footer -->
+				<div class="modal-footer ">
+				<button type="button" class="btn color-botom-home-inline sombreado" data-dismiss="modal">Cerrar</button>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Modal Perfil para barbero -->
+	<div class="modal" id="myModal04">
+		<div class="modal-dialog modal-large">
+			<div class="modal-content letras ">
+
+				<!-- Modal Header -->
+				<div class="modal-header">
+				<h4 class="modal-title"><b>Perfil</b></h4>
+				<button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
+				</div>
+
+				<!-- Modal body -->
+				<div class="modal-body text-justify">
+				  <h4><p>Como usuario-barbero, éste enlace lo llevará a la vista donde podrá ver todo su perfil.</p></h4>
+				</div>
+
+				<!-- Modal footer -->
+				<div class="modal-footer ">
+				<button type="button" class="btn color-botom-home-inline sombreado" data-dismiss="modal">Cerrar</button>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Modal Perfil para clientes -->
+	<div class="modal" id="myModal004">
+		<div class="modal-dialog modal-large">
+			<div class="modal-content letras ">
+
+				<!-- Modal Header -->
+				<div class="modal-header">
+				<h4 class="modal-title"><b>Perfil</b></h4>
+				<button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
+				</div>
+
+				<!-- Modal body -->
+				<div class="modal-body text-justify">
+				  <h4><p>Como usuario-cliente, éste enlace lo llevará a la vista donde podrá ver todo su perfil.</p></h4>
+				</div>
+
+				<!-- Modal footer -->
+				<div class="modal-footer ">
+				<button type="button" class="btn color-botom-home-inline sombreado" data-dismiss="modal">Cerrar</button>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Modal editar Perfil para barberos -->
+	<div class="modal" id="myModal05">
+		<div class="modal-dialog modal-large">
+			<div class="modal-content letras ">
+
+				<!-- Modal Header -->
+				<div class="modal-header">
+				<h4 class="modal-title"><b>Perfil</b></h4>
+				<button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
+				</div>
+
+				<!-- Modal body -->
+				<div class="modal-body text-justify">
+				  <h4><p>Como usuario-barbero, éste enlace lo llevará a la vista donde podrá editar la información básica.</p></h4>
+				</div>
+
+				<!-- Modal footer -->
+				<div class="modal-footer ">
+				<button type="button" class="btn color-botom-home-inline sombreado" data-dismiss="modal">Cerrar</button>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	
+	
+	<!-- Modal editar Perfil para clientes -->
+	<div class="modal" id="myModal005">
+		<div class="modal-dialog modal-large">
+			<div class="modal-content letras ">
+
+				<!-- Modal Header -->
+				<div class="modal-header">
+				<h4 class="modal-title"><b>Perfil</b></h4>
+				<button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
+				</div>
+
+				<!-- Modal body -->
+				<div class="modal-body text-justify">
+				  <h4><p>Como usuario-cliente, éste enlace lo llevará a la vista donde podrá editar la información básica.</p></h4>
+				</div>
+
+				<!-- Modal footer -->
+				<div class="modal-footer ">
+				<button type="button" class="btn color-botom-home-inline sombreado" data-dismiss="modal">Cerrar</button>
+				</div>
+
+			</div>
+		</div>
+	</div>
