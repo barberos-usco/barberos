2f0b954d0a238293b6dbfbcf072263f0f9217991
Byakoxx <thetorrez5@hotmail.com>
2020-08-14 17:58:47 -0500

    Add: Modal del editar especialidad, falta implementacion. -Helmer

--- resources/views/barbero/users/editar_especialidad.blade.php
+++ resources/views/barbero/users/editar_especialidad.blade.php
@@ -106,11 +106,85 @@
             </div>
         </div>
 
+        <div class="caja-especialidad container sombreado">
+            <div class="card-body ">
+                {{-- {!!Form::model($user,['method'=>'PATCH', 'action'=>['BarberoUsersController@update', $user->id], 'files'=>true], ['class'=>'form-inline']) !!}--}}
+                    <div class="row">
+                        <div class="columna letras" >
+                            <img src="{{ asset('/images/especialidad-izq.png') }}" class="img-fluid  " alt="Responsive image" width="100%" >
+                        </div>
+                        <div class="columna letras" >
+                            <div class="section-title">
+                                <h2>Editar Especialidades |
+                                    <div class="btn color-manual redondeo sombreado" data-toggle="modal" data-target="#myModal2"> ?</div>
+                                    <!-- The Modal para barberos -->
+                                    <div class="modal" id="myModal2">
+                                        <div class="modal-dialog modal-large">
+                                            <div class="modal-content letras ">
+            
+                                                <!-- Modal Header -->
+                                                <div class="modal-header">
+                                                <h4 class="modal-title"><b>Editar Especialidades</b></h4>
+                                                <button type="button" class="close blanco-close" data-dismiss="modal">&times;</button>
+                                                </div>
+            
+                                                <!-- Modal body -->
+                                                <div class="modal-body text-justify">
+                                                <h4><p>En esta vista, podremos hacer elección de las especialidades que queremos tener en el perfil usuario-barbero, de las que se seleccione aquí, se le activarán en "editar servicio por especialidad", donde podrá ser más específico. </p></h4>
+                                                </div>
+            
+                                                <!-- Modal footer -->
+                                                <div class="modal-footer ">
+                                                <button type="button" class="btn color-botom-home-inline sombreado" data-dismiss="modal">Cerrar</button>
+                                                </div>
+            
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                </h2>
+                            </div>
+                            <div>
+                               {!! Form::checkbox('Peluqueria', 'Peluqueria') !!} Peluqueria
+                            </div>
+                            <div>
+                               {!! Form::checkbox('Rasurado', 'Rasurado') !!} Rasurado
+                            </div>
+                            <div>
+                               {!! Form::checkbox('Masajes y faciales', 'Masajes y faciales') !!} Masajes y faciales
+                            </div>
+                            <div>
+                               {!! Form::checkbox('Cejas', 'Cejas') !!} Cejas
+                            </div>
+                            <div>
+                               {!! Form::checkbox('Reporalizacion', 'Reporalizacion') !!} Reporalización
+                            </div>
+                            <div>
+                               {!! Form::checkbox('Tratamiento Capilar', 'Tratamiento Capilar') !!} Tratamiento Capilar
+                            </div>
+                            <div>
+                               {!! Form::checkbox('Depilación con Cera', 'Depilación con Cera') !!} Depilación con Cera
+                            </div><br><br>
+                            {!! Form::submit('Guardar', ['class'=>'btn color-botom sombreado centrado']) !!}
+                            {!! Form::reset('Borrar', ['class'=>'btn color-botom-borrar sombreado']) !!}
+                        </div>
+                        <div class="columna letras" >
+                            <img src="{{ asset('/images/especialidad-dere.png') }}" class="img-fluid  " alt="Responsive image" width="100%" >
+                        </div>
 
-        
-                        
-        
+
+                        {{--  <div class="column letras" >
+                            
+                            <div>
+                                {!! Form::submit('Guardar', ['class'=>'btn color-botom sombreado centrado']) !!}
+                            </div>
+                        </div>--}}
+                    </div>
+                {!! Form::close() !!}</br>  
+            </div>
+        </div>
+
     </div>
-    
+
 </main>
 @endsection
